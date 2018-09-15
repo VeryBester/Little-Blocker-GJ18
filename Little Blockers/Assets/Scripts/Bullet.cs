e26f0c569e7d34aec49aa5fdbd1dcf09973db411
VeryBester <david15202@gmail.com>
2018-09-15 16:10:13 -0500

    Added New Objects and Scripts
    Created and added bullet object, spawner object, and scripts for both.

---
+++ Little Blockers/Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    // Use this for initialization
+    Rigidbody2D rb;
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
