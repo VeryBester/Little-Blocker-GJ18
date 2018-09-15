8f87f62e32f4be31ee7d147f8289b201980cf6f5
Robert Guan <guan.robert@utexas.edu>
2018-09-15 16:08:35 -0500

    basic movement (no collision yet)

--- Little Blockers/Assets/Scripts/PlayerMovement.cs
+++ Little Blockers/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float speed = 10.0f;             //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb2d;
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        //Store the current horizontal input in the float moveHorizontal.
+        //float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        //float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        rb2d.velocity = movement;
+    
+    }
 }
