2306239aab0bc2e05f7b16dfe32c08e27f129654
Robert Guan <guan.robert@utexas.edu>
2018-09-16 16:01:26 -0500

    added assets, bounce mechanic working

--- Little Blockers/Assets/Scripts/PlayerMovement2.cs
+++ Little Blockers/Assets/Scripts/PlayerMovement2.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement2 : MonoBehaviour
+{
 
     public float speed = 10.0f;             //Floating point variable to store the player's movement speed.
 
@@ -25,33 +26,34 @@ public class PlayerMovement : MonoBehaviour {
         */
         //transform.Rotate(0, 0, rotationSpeed * Time.deltaTime); 
 
-        
+
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
 
-        if (Input.GetKey(KeyCode.Comma)){
+        if (Input.GetKey(KeyCode.F))
+        {
             transform.Rotate(0, 0, -rotationSpeed);
         };
-        if (Input.GetKey(KeyCode.Period))
+        if (Input.GetKey(KeyCode.G))
         {
             transform.Rotate(0, 0, rotationSpeed);
         };
         //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis("Horizontalarrows");
+        float moveHorizontal = Input.GetAxis("Horizontalletters");
 
         //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis("Verticalarrows");
-        
+        float moveVertical = Input.GetAxis("Verticalletters");
+
         //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         //rb2d.AddForce(movement * speed);
         rb2d.velocity = movement * speed;
-        
+
         //transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
     }
 }
