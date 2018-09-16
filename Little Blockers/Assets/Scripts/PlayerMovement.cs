f63a17b7c9c7231fe0cd2f3abffcc5bd3f0628df
Robert Guan <guan.robert@utexas.edu>
2018-09-16 14:42:30 -0500

    new shield control

--- Little Blockers/Assets/Scripts/PlayerMovement.cs
+++ Little Blockers/Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,7 @@ public class PlayerMovement : MonoBehaviour {
     public float speed = 10.0f;             //Floating point variable to store the player's movement speed.
 
     private Rigidbody2D rb2d;
-    private float rotationSpeed = 120;
+    private float rotationSpeed = 4 ;
     // Use this for initialization
     void Start()
     {
@@ -21,18 +21,22 @@ public class PlayerMovement : MonoBehaviour {
         var pos = Camera.main.WorldToScreenPoint(transform.position);
         var dir = Input.mousePosition - pos;
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        */  
+        */
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
 
-        
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
 
-        if (Input.GetKeyDown(KeyCode.X)){
-            rotationSpeed  = rotationSpeed * -1;
-        };
+        if (Input.GetKey(KeyCode.Z)){
+            transform.Rotate(0, 0, rotationSpeed);
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            transform.Rotate(0, 0, -rotationSpeed);
+        }
         //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
@@ -45,6 +49,6 @@ public class PlayerMovement : MonoBehaviour {
         //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         //rb2d.AddForce(movement * speed);
         rb2d.velocity = movement * speed;
-        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
+        
     }
 }
